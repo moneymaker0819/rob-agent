e81a0375de4dfb63df7ecbc1b279ae4678fb97ff
moneymaker0819 <robr139@gmail.com>
2026-09-19 18:09:56 -0400

    Correct Flask app syntax and improve structure

--- app.py
+++ app.py
@@ -1,18 +1,27 @@
 from flask import Flask, request
-
-app=Flask(_name_)
-@app.route("/")
-def home():
-    return "UFE Running"
-
-@app.route("/webhook", methods=["POST"])
-6
-def webhook():
-7
-print("Headers:", request.headers)
-8
-print("Raw Data:", request.data)
-9
+2
  
+3
+app = Flask(__name__)
+4
+ 
+5
+@app.route("/")
+6
+def home():
+7
+return "UFE Running"
+8
+ 
+9
+@app.route("/webhook", methods=["POST"])
 10
+def webhook():
+11
+print("Headers:", request.headers)
+12
+print("Raw Data:", request.data)
+13
+ 
+14
 return {"status": "received"}, 200
