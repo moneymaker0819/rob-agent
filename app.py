b0be5e1242b322bab88d3c6113354a859534b373
moneymaker0819 <robr139@gmail.com>
2026-09-19 18:23:06 -0400

    Adjust formatting of webhook function in app.py

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 @app.route("/webhook", methods=["POST"])
-2
+
 def webhook():
-3
+
 print("Headers:", request.headers)
-4
+
 print("Raw Data:", request.data)
-5
+
 return {"status": "received"}, 200
