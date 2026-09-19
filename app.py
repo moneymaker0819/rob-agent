2cf528b1bd01ce3a53962d29252b874301009737
moneymaker0819 <robr139@gmail.com>
2026-09-19 17:40:48 -0400

    Refactor app.py for improved formatting

--- app.py
+++ app.py
@@ -1,27 +1,19 @@
-from flask import Flask, request
-2
- 
-3
+from flask import Flask,
+request 
+
 app = Flask(__name__)
-4
  
-5
 @app.route("/")
-6
 def home():
-7
-return "UFE Running"
-8
- 
-9
-@app.route("/webhook", methods=["POST"])
-10
+    return "UFE Running"
+
+@app.route("/webhook",
+methods=["POST"])
 def webhook():
-11
-print("Headers:", request.headers)
-12
-print("Raw Data:", request.data)
-13
- 
-14
-return {"status": "received"}, 200
+    print("Headers:",
+request.headers)
+    print("Raw Data:",
+request.data)
+
+    return {"status":
+"received"}, 200
